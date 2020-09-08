e6de6ae47e1b8ff915c8884cae41751dedfd4caa
smevel <stephane.mevel-viannay@bretagne.bzh>
2020-09-08 14:24:25 +0200

    commit 0.4

--- themes/vex-hugo/assets/script.js
+++ themes/vex-hugo/assets/script.js
@@ -33,7 +33,25 @@ $(document).ready(function () {
         type: 'image'
     });
 
+    /* ========================================================================= */
+  	/*	Portfolio Filtering Hook
+  	/* =========================================================================  */
 
+  	var containerEl = document.querySelector('.shuffle-wrapper');
+  	if (containerEl) {
+  		var Shuffle = window.Shuffle;
+  		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+  			itemSelector: '.shuffle-item',
+  			buffer: 1
+  		});
+
+  		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+  			var input = evt.currentTarget;
+  			if (input.checked) {
+  				myShuffle.filter(input.value);
+  			}
+  		});
+  	}
 
     // animation scroll js
     var html_body = $('html, body');
